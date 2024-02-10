cb1ed55cea32b58f765c5988254bf2671342a4f0
Denis <bulavatsky@list.ru>
2024-02-10 17:44:26 +0300

    uncommit CI/CD

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,14 +1,14 @@
-const webpack = require("../webpack.prod");
-const WebpackDevServer = require("../webpack.dev");
-const config = require("../webpack.dev");
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import config from '../webpack.config';
 
 const server = new WebpackDevServer(webpack(config), {});
-server.listen(9000, "localhost", (err) => {
+server.listen(9000, 'localhost', (err) => {
   if (err) {
     console.error(err);
     return;
   }
   if (process.send) {
-    process.send("ok");
+    process.send('ok');
   }
 });
